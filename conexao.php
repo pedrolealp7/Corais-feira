b9e201b9d7d111a8cbe54852b2234a207ca48318
Pedro r Leal <pedro08leal@gmail.com>
2025-05-19 09:23:45 -0300

    arrumei algumas coisas
    isso

---
+++ conexao.php
@@ -0,0 +1,12 @@
+<?php
+// dados do servidor de banco de dados
+$host = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "corais";
+
+// objeto que controla a conexao com o banco
+$conn = new mysqli($host, $usuario, $senha, $banco);
+
+
+?>
