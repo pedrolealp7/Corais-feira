d2242afd22034be080ccb5bbd694b565eea55bd4
Pedro r Leal <pedro08leal@gmail.com>
2025-05-27 09:00:46 -0300

    atualizacao

--- conexao.php
+++ conexao.php
@@ -8,5 +8,9 @@ $banco = "corais";
 // objeto que controla a conexao com o banco
 $conn = new mysqli($host, $usuario, $senha, $banco);
 
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
 
+echo "Conexão bem-sucedida!";
 ?>
