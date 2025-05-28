c2a53d7cc15cc84b900b662c2141da7d249d4c2f
Pedro r Leal <pedro08leal@gmail.com>
2025-05-28 08:55:56 -0300

    cards criados

--- recifes.php
+++ recifes.php
@@ -3,7 +3,51 @@ include_once './includes/conexao.php';
 include_once './includes/header.php';
 ?>
 <main>
+<div class="info1">
+      <h2>Recife de franja</h2>
+      <p>Esse coral tem um padrão que lembra um cérebro humano e é encontrado no Caribe.</p>
+      <div class="foto1">
+        <img src="https://lh5.googleusercontent.com/proxy/jTB0DvDumRDJ5-8zR6Qdr3mgrjSECjm5VxEdtxvQMjgCO_I6cXRR-GLjUjwK3W3clNFXwMHLU8Lvz0v6ux4" alt="">
+        <a href="corais.html"> </a>
+        
+      </div>
+    </div>
 
+    <div class="info1">
+        <h2>Recife-Barreira</h2>
+        <p>O maior recife de coral vivo do mundo é a Grande Barreira de Corais na Austrália, que pode ser vista do espaço. </p>
+        <div class="foto1">
+          <img src="https://www.aquaa3.com.br/wp-content/uploads/2014/06/Grande-Barreira-de-Coral-1024x768.jpg" alt="Imagem de um coral bonito"> 
+        </div>
+      </div>
+
+      <div class="info1">
+        <h2>Recife Atol</h2>
+        <p>O maior recife de coral vivo do mundo é a Grande Barreira de Corais na Austrália, que pode ser vista do espaço. </p>
+        <div class="foto1">
+          <img src="https://marsemfim.com.br/wp-content/uploads/2018/01/corais-1-.jpg" alt="Imagem de um coral bonito"> 
+        </div>
+      </div>
+
+      <div class="info1">
+      <h2>Recife de plataforma</h2>
+      <p>Esse coral tem um padrão que lembra um cérebro humano e é encontrado no Caribe.</p>
+      <div class="foto1">
+        <img src="https://static.poder360.com.br/2023/09/oceano-recifeartificial-petroliferas-848x477.png" alt="">
+        <a href="corais.html"> </a>
+        
+      </div>
+    </div>
+
+    <div class="info1">
+      <h2>Recifes Bancais</h2>
+      <p>Esse coral tem um padrão que lembra um cérebro humano e é encontrado no Caribe.</p>
+      <div class="foto1">
+        <img src="https://peldcom.eco.br/wp-content/uploads/2022/05/FIG-1.1-Imagem-aerea-do-Arco-Costeiro-do-Banco-de-Abrolhos-Foto-F.jpg" alt="">
+        <a href="corais.html"> </a>
+        
+      </div>
+    </div>
 </main>
 <?php
 include_once './includes/footer.php';
