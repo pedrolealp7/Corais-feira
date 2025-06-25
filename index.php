eca80a40eaf2841ba03b9d2d56d5fcbf91e5a623
Pedro r Leal <pedro08leal@gmail.com>
2025-06-25 14:45:46 -0300

    ok

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 
 <?php
-include_once './includes/conexao.php';
-include_once './includes/header.php';
+include_once(__DIR__ . '/includes/conexao.php');
+include_once(__DIR__ . '/includes/header.php');
+
 ?>
 </header>
   <main>
